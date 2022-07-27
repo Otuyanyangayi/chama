547bf4ab163e189c5fe420ed7f5a84ef0927b176
Grace <gracenyangayi@gmail.com>
2022-07-27 20:19:56 +0300

    updates

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -29,13 +29,13 @@ export const AppReducer = (state, action) => {
 
 // 1. Sets the initial state when the app loads
 const initialState = {
-	budget: 2000,
+	budget: 6000,
 	expenses: [
-		{ id: uuidv4(), name: 'Shopping', cost: 50 },
-		{ id: uuidv4(), name: 'Holiday', cost: 300 },
-		{ id: uuidv4(), name: 'Transportation', cost: 70 },
-		{ id: uuidv4(), name: 'Fuel', cost: 40 },
-		{ id: uuidv4(), name: 'Child Care', cost: 500 },
+		{ id: uuidv4(), name: 'grace', cost: 50 },
+		{ id: uuidv4(), name: 'Audrey', cost: 300 },
+		{ id: uuidv4(), name: 'Agatha', cost: 70 },
+		{ id: uuidv4(), name: 'lydia', cost: 40 },
+		{ id: uuidv4(), name: 'Achieng', cost: 500 },
 	],
 };
 
