8c314aae1a826e7b306d201d6f8aaf855525db3a
Grace <gracenyangayi@gmail.com>
2022-08-02 18:10:02 +0300

    updates

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -31,7 +31,7 @@ export const AppReducer = (state, action) => {
 const initialState = {
 	budget: 6075,
 	expenses: [
-		{ id: uuidv4(), name: 'Achieng -0', cost: 0 },
+		{ id: uuidv4(), name: 'Achieng -1515', cost: 1515 },
 		{ id: uuidv4(), name: 'Lydia -0', cost: 0 },
 		{ id: uuidv4(), name: 'Otuya -1515', cost: 1515 },
 		{ id: uuidv4(), name: 'Audrey -1515', cost: 1515 },
