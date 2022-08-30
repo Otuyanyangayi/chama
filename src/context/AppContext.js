9f6ef14f917e8c6631bb34c9ab3e1af45caa78bd
Grace <gracenyangayi@gmail.com>
2022-08-30 14:28:33 +0300

    updates

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -34,7 +34,7 @@ const initialState = {
 		{ id: uuidv4(), name: 'Achieng -0', cost: 0 },
 		{ id: uuidv4(), name: 'Lydia - 0', cost: 0 },
 		{ id: uuidv4(), name: 'Otuya -To be paid to', cost: 15 },
-		{ id: uuidv4(), name: 'Audrey -0', cost: 0 },
+		{ id: uuidv4(), name: 'Audrey -1515', cost: 1515 },
 		{ id: uuidv4(), name: 'Agatha -500', cost:500 },
 	],
 };
